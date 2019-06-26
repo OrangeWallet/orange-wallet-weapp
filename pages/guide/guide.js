18a110a78ea1fc493afa593894093ea76cdef302
Bao Jun <baojun.cz@gmail.com>
2019-06-26 15:15:51 +0800

    feat: guide page

---
+++ pages/guide/guide.js
@@ -0,0 +1,26 @@
+var Sercurity = "/images/security.svg";
+var Privacy = "/images/privacy.svg";
+var Public = "/images/public.svg";
+Page({
+  data: {
+    swiper_items: [
+      {
+        title: "安全",
+        text: "绝不保存私钥，仅提供查询功能",
+        icon: Sercurity
+      },
+      {
+        title: "隐私",
+        text: "不会收集用户隐私信息，现在不会，将来也不会",
+        icon: Privacy
+      },
+      {
+        title: "开源",
+        text: "代码在Github，欢迎贡献",
+        icon: Public
+      }
+    ]
+  },
+  createWallet: () => {},
+  importWallet: () => {}
+});
---
+++ pages/index/index.js
@@ -0,0 +1,7 @@
+Page({
+  guide: function() {
+    wx.navigateTo({
+      url: "../guide/guide"
+    });
+  }
+});
