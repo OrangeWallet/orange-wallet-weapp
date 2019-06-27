997f1c3c8e7ee2c1e5161341a96c2ceabd010f7c
Bao Jun <baojun.cz@gmail.com>
2019-06-27 18:52:21 +0800

    feat: import_wallet ui

--- pages/import_wallet/import_wallet.js
+++ pages/import_wallet/import_wallet.js
@@ -1,8 +1,31 @@
 Page({
   data: {
-    tabActive: 0
+    tabActive: 0,
+    observerChecked: false,
+    observerDisable: false,
+    key: "",
+    password: "",
+    repassword: ""
   },
   onTabChange: function(event) {
-    this.setData({ tabActive: event.detail });
-  }
+    let observerChecked = this.data.observerChecked;
+    let observerDisable = this.data.observerDisable;
+    if (event.detail == 1) {
+      observerChecked = true;
+      observerDisable = true;
+    } else {
+      observerChecked = false;
+      observerDisable = false;
+    }
+    this.setData({
+      tabActive: event.detail,
+      key: "",
+      observerChecked,
+      observerDisable
+    });
+  },
+  onObserverChanged: function(event) {
+    this.setData({ observerChecked: event.detail });
+  },
+  importWallet: () => {}
 });
