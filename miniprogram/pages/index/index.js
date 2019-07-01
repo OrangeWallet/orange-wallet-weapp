fd32575d793bbff4a6af72817d5eedefe08bb328
Bao Jun <baojun.cz@gmail.com>
2019-07-01 19:48:28 +0800

    refactor: remove password valid when open app

--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -3,22 +3,15 @@ const app = getApp();
 Page({
   onReady: function() {
     const WalletUtils = require("../../utils/wallet_utils");
-    const page = this;
     WalletUtils.readPublicKey({
       success: publicKey => {
         WalletUtils.isObserverModel({
           success: isObserver => {
             app.globalData.publicKey = publicKey;
             app.globalData.isOberverModel = isObserver;
-            if (isObserver) {
-              wx.redirectTo({
-                url: "../home/home"
-              });
-            } else {
-              page.setData({
-                showModel: true
-              });
-            }
+            wx.redirectTo({
+              url: "../home/home"
+            });
           },
           fail: () => {
             wx.showModal({
@@ -37,18 +30,5 @@ Page({
         }, 1000);
       }
     });
-  },
-  data: {
-    showModel: false,
-    passwordErrorMessage: ""
-  },
-  onVerifyCorrect: function(event) {
-    const wallet = event.detail.wallet;
-    app.globalData.publicKey = wallet.publicKey;
-    app.globalData.isOberverModel = wallet.privateKey === "";
-    wx.redirectTo({
-      url: "../home/home"
-    });
-  },
-  onCancel: function() {}
+  }
 });
